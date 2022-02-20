45fce2eb3424572881e9adf8b3085a42fe50d864
Alex Leontiev <alozz1991@gmail.com>
2022-02-20 12:58:34 +0900

    fixup

--- run.sh
+++ run.sh
@@ -2,9 +2,10 @@
 
 VOLUME_NAME=named-volume
 DUMMY_CONTAINER_NAME=dummy
+DOCKER_EXEC=/snap/bin/docker
 
-docker run --shm-size 1G --rm -v $VOLUME_NAME:/screenshots:rw nailbiter/puppeteer pdf_screenshot 'https://www.jma.go.jp/bosai/forecast/' 800x600 2000
-docker run -d --rm --name $DUMMY_CONTAINER_NAME -v $VOLUME_NAME:/root alpine tail -f /dev/null
-docker cp $DUMMY_CONTAINER_NAME:/root/screenshot_800_600.pdf pdfs/screencap-`date "+%Y-%m-%d-%H-%M"`.pdf
-docker stop dummy
+$DOCKER_EXEC run --shm-size 1G --rm -v $VOLUME_NAME:/screenshots:rw nailbiter/puppeteer pdf_screenshot 'https://www.jma.go.jp/bosai/forecast/' 800x600 2000
+$DOCKER_EXEC run -d --rm --name $DUMMY_CONTAINER_NAME -v $VOLUME_NAME:/root alpine tail -f /dev/null
+$DOCKER_EXEC cp $DUMMY_CONTAINER_NAME:/root/screenshot_800_600.pdf pdfs/screencap-`date "+%Y-%m-%d-%H-%M"`.pdf
+$DOCKER_EXEC stop dummy
 ls pdfs
